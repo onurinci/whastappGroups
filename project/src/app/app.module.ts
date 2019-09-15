d29fb8d87b59e1b303e115d5548e5e2961fd2a59
Onur İNCİ <onur.nc@gmail.com>
2019-09-15 22:13:19 +0300

    create services

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { GlobalService } from './services/global.service';
+import { ApiService } from './services/api.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,10 @@ import { GlobalService } from './services/global.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [GlobalService],
+  providers: [GlobalService, ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ project/src/app/services/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl: string = "";
+  constructor(private _http: HttpClient) { }
+
+  getSliders(): Observable<any[]> {
+    return this._http.get<any[]>(`${this.baseUrl}/api/apiData/GetSlidersList`);
+  }
+
+  register(formData: any): Observable<number> {
+
+    const httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+
+    return this._http.post<number>(this.baseUrl + "api/apiData/Register", formData, httpOptions);
+}
+
+}
